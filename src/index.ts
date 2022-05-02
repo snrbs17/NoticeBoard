f1012c851f9febcafa30e19dc13c7e4e9a470a61
LJH <snrbs17@gmail.com>
2022-05-02 13:51:42 +0900

    event controller promise 적용

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import { Popup } from './popup';
 type eventController = {
 	x: number;
 	y: number;
-	state: 'running' | 'pending' | 'wait';
+	state: (params: any) => Promise<any>;
+	// state: 'running' | 'pending' | 'wait';
 	content?: string;
 };
 const TIME_TO_READ: number = 2000;
@@ -23,45 +24,64 @@ class App {
 		this.canvas = document.createElement('canvas');
 		this.canvas.setAttribute('style', 'width: 100%; height: 100%; background-color: #222');
 		this.canvas.onclick = this.clickCanvas.bind(this);
-		this.canvas.addEventListener(
-			'closePopup',
-			() => {
-				this.showDocument.state = 'wait';
-			},
-			true,
-		);
 		this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
 
-		this.stageWidth = 0;
-		this.stageHeight = 0;
-
-		this.showDocument = { x: 0, y: 0, state: 'wait' };
+		this.stageWidth = window.innerWidth;
+		this.stageHeight = window.innerHeight;
+		this.canvas.width = this.stageWidth;
+		this.canvas.height = this.stageHeight;
 
 		document.body.appendChild(this.canvas);
 
-		window.addEventListener('resize', this.resize.bind(this), false);
-		this.resize();
+		this.showDocument = { x: 0, y: 0, state: () => new Promise((res, rej) => {}) };
 
 		this.noticeBoard = new NoticeBoard(this.ctx, this.stageWidth, this.stageHeight);
 		this.memoList = [];
 		let i = 1;
-		fetch('http://localhost:3000/dbFind')
-			.then((response) => response.json())
-			.then((res) =>
-				res.forEach((memo: any) => {
-					if (memo?.url) {
-						this.memoList.push(new Memo(memo.name, this.ctx, { x: 300 * i++, y: 200 }, 200));
-					}
-				}),
-			);
+		// fetch('http://localhost:3000/dbFind')
+		// 	.then((response) => response.json())
+		// 	.then((res) =>
+		const res = [
+			{ name: 'AI', url: 'AI.html' },
+			{ name: 'css', url: 'css.html' },
+			{ name: 'html', url: 'html.html' },
+			{ name: 'docker', url: 'docker.html' },
+		];
+		res.forEach((memo: any) => {
+			if (memo?.url) {
+				this.memoList.push(new Memo(memo.name, this.ctx, { x: 300 * i++, y: 200 }, 200));
+			}
+		}),
+			// );
+
+			this.canvas.addEventListener('openPopup', () => {
+				this.handlePopupPendingState();
+			});
+		this.canvas.addEventListener(
+			'closePopup',
+			() => {
+				// need to update promise
+				this.showDocument.state = () => new Promise((res, rej) => {});
+			},
+			true,
+		);
 
 		requestAnimationFrame(this.animate.bind(this));
 	}
+
 	clickCanvas(event: MouseEvent): void {
 		if (!event) return;
 		const x = event.clientX;
 		const y = event.clientY;
-		if (this.showDocument.state == 'wait') this.showDocument = { x, y, state: 'pending' };
+		const state = (selectedMemo: string) =>
+			new Promise((res, rej) => {
+				selectedMemo ? res(console.log('success')) : rej(console.log('fail'));
+			});
+		this.showDocument = { x, y, state };
+
+		document.querySelector('canvas')?.dispatchEvent(new Event('openPopup', { bubbles: true }));
+
+		this.handlePopupPendingState;
 	}
 
 	handlePopupPendingState() {
@@ -73,36 +93,28 @@ class App {
 		});
 		const selectedMemo = selectedMemoCandidates.find((x) => !!x);
 		console.log(selectedMemo);
-		if (!selectedMemo) {
-			this.showDocument.state = 'wait';
-			return;
-		}
-		this.showDocument.content = selectedMemo;
-		this.popup = new Popup(selectedMemo);
-		this.showDocument.state = 'running';
-		setTimeout(() => {
-			this.requestDBInsert(selectedMemo);
-		}, TIME_TO_READ);
-	}
-	requestDBInsert(name: string): void {
-		if (this.showDocument.state === 'running') {
-			fetch('http://localhost:3000/dbInsert', {
-				method: 'POST',
-				mode: 'cors',
-				cache: 'no-cache',
-				headers: { 'Content-Type': 'application/json' },
-				body: `{"name": "${name}"}`,
+		this.showDocument
+			.state(selectedMemo)
+			.then(() => {
+				this.popup = new Popup(selectedMemo as string);
+
+				setTimeout(() => {
+					this.requestDBInsert(selectedMemo as string);
+				}, TIME_TO_READ);
+			})
+			.catch((e) => {
+				console.log(e);
 			});
-		}
 	}
 
-	resize(): void {
-		this.stageWidth = window.innerWidth;
-		this.stageHeight = window.innerHeight;
-		this.canvas.width = this.stageWidth;
-		this.canvas.height = this.stageHeight;
-
-		// this.noticeBoard?.resize(this.stageWidth, this.stageHeight);
+	requestDBInsert(name: string): void {
+		fetch('http://localhost:3000/dbInsert', {
+			method: 'POST',
+			mode: 'cors',
+			cache: 'no-cache',
+			headers: { 'Content-Type': 'application/json' },
+			body: `{"name": "${name}"}`,
+		});
 	}
 
 	animate(t: any): void {
@@ -110,10 +122,6 @@ class App {
 
 		this.noticeBoard.draw();
 		this.memoList.forEach((memo) => memo.draw(t));
-
-		if (this.showDocument.state === 'pending') {
-			this.handlePopupPendingState();
-		}
 
 		requestAnimationFrame(this.animate.bind(this));
 	}
